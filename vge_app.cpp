38e0c30f25b082a5b8e9db7df1ed1f086206b25f
1337est <59178218+1337est@users.noreply.github.com>
2024-09-11 12:07:59 -0500

    added point lighting and basic floor obj

--- vge_app.cpp
+++ vge_app.cpp
@@ -24,10 +24,11 @@ namespace vge
 
 struct GlobalUbo // Uniform buffer object
 {
-    alignas(16) glm::mat4 projectionView{ 1.f };
-    alignas(16) glm::vec3 lightDirection = glm::normalize(glm::vec3{ 1.f,
-                                                                     -3.f,
-                                                                     -1.f });
+    glm::mat4 projectionView{ 1.f };
+
+    glm::vec4 ambientLightColor{ 1.f, 1.f, 1.f, .02f }; // w is intensity
+    glm::vec3 lightPosition{ -1.f };
+    alignas(16) glm::vec4 lightColor{ 1.f }; // w is light intensity
 };
 
 VgeApp::VgeApp()
@@ -88,6 +89,7 @@ void VgeApp::run()
     camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f));
 
     auto viewerObject = VgeGameObject::createGameObject();
+    viewerObject.m_transform.translation.z = -2.5f;
     VgaKeyboardMovementController cameraController{};
 
     auto currentTime = std::chrono::high_resolution_clock::now();
@@ -113,7 +115,8 @@ void VgeApp::run()
             viewerObject.m_transform.rotation);
 
         float aspect = m_vgeRenderer.getAspectRatio();
-        camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
+        camera
+            .setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 100.f);
 
         // beginFrame returns nullptr if swapchain needs to be recreated
         if (auto commandBuffer = m_vgeRenderer.beginFrame())
@@ -150,7 +153,7 @@ void VgeApp::loadGameObjects()
         VgeModel::createModelFromFile(m_vgeDevice, "models/flat_vase.obj");
     auto flatVase = VgeGameObject::createGameObject();
     flatVase.m_model = vgeModel;
-    flatVase.m_transform.translation = { -.5f, .5f, 2.5f };
+    flatVase.m_transform.translation = { -.5f, .5f, 0.f };
     flatVase.m_transform.scale = { 3.f, 1.5f, 3.f };
     m_gameObjects.push_back(std::move(flatVase));
 
@@ -158,9 +161,16 @@ void VgeApp::loadGameObjects()
         VgeModel::createModelFromFile(m_vgeDevice, "models/smooth_vase.obj");
     auto smoothVase = VgeGameObject::createGameObject();
     smoothVase.m_model = vgeModel;
-    smoothVase.m_transform.translation = { .5f, .5f, 2.5f };
+    smoothVase.m_transform.translation = { .5f, .5f, 0.f };
     smoothVase.m_transform.scale = { 3.f, 1.5f, 3.f };
     m_gameObjects.push_back(std::move(smoothVase));
+
+    vgeModel = VgeModel::createModelFromFile(m_vgeDevice, "models/quad.obj");
+    auto floor = VgeGameObject::createGameObject();
+    floor.m_model = vgeModel;
+    floor.m_transform.translation = { 0.f, .5f, 0.f };
+    floor.m_transform.scale = { 3.f, 1.f, 3.f };
+    m_gameObjects.push_back(std::move(floor));
 }
 
 } // namespace vge
